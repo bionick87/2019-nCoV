ac0263518b98490fb8cc71af6b39156a26543d0d
Nicolò Savioli <nicolo.l.savioli@kcl.ac.uk>
2020-02-24 10:18:43 +0100

    first

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# Artificial Intelligence model for the analysis of COVID-2019
+# Artificial Intelligence model for the analysis of COVID-2019 
 
 ## Purpose 
 
 Deep learning application to track COVID-2019 virus evolution by analyzing the RNA sequence between two COVID-2019 samples at different time points.
-The network is based on a deep Siamese Neural Network (https://en.wikipedia.org/wiki/Siamese_neural_network) that takes a strand of COVID-2019 sequences as input. More specifically, each strand is converted into a grayscale image of 256x256 pixel size and passed through two convolutions convolutional networks (see at the plot below). However, this DeepNet is trained towards negative examples with Human Immunodeficiency Viruses (HIV) strand examples.
+
+The network is based on a deep Siamese Neural Network (https://en.wikipedia.org/wiki/Siamese_neural_network) that takes a strand of COVID-2019 sequences as input.
+
+More specifically, each strand is converted into a grayscale image of 256x256 pixels size and passed through two  convolutions AlexNet (https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf) Convolutional Networks (CNN). However, both CNN are trained towards negative examples with Human Immunodeficiency Viruses (HIV) strand examples (i.e how it is a biologically different virus).
+
 The aim is to find nucleotide sequences that stably characterizes COVID-2019 for finding a specific RNA biomarker strand.
+In particular, it might be useful to compare the variation over time of thex the densely glycosylated spike (Sprotein sequences which coronavirus uses to enter in the host cell (https://science.sciencemag.org/content/early/2020/02/19/science.abb2507/tab-pdf).
+
+This work is in progress and since I do it in my spare time - I will give new constant updates to the code.
+However, understanding the urgency of the phenomenon, I decided to make the code available to give new analysis tools to the international scientific community. This code can only be used for scientific purposes.
+
 
 ### Wuhan-Hu-1 isolated coronavirus 2 complete genome sequences have been downloaded from link below and saved in the project folder ./virus_genome
 
@@ -22,12 +31,16 @@ The code is released for research purposes only and not for commercial purposes.
 
 ## USE
 
-* 
+* Generation of the dataset (COVID-2019 vs HIV) - go to ./get_dataset folder then open main file change the paths of the HIV and COVID-2019 sequences and the folder where to save the dataset.
+
 
 
 ## TODO
-  * cluster testing with pairwise 
-  * Convert into nucleotide bases in an image to pass it towards a convolutional network.
+
+
+
+
+
 
 
 ### Prerequisites
--- get_dataset/main.py
+++ get_dataset/main.py
@@ -4,19 +4,26 @@ import text_to_image
 import os
 from tqdm import tqdm
 import cv2
+from   sklearn.model_selection import train_test_split
+
 
 def readGenSeq(path_file):
     with open(path_file, 'r') as f:
         data = f.read()
     return data
 
+def SplitStrands(seq,nstrands):
+    seq_txt         = readGenSeq(seq)
+    nstrandsList    = wrap(seq_txt,nstrands)
+    train, tmp_test = train_test_split(nstrandsList, test_size=0.4, random_state=42)
+    test,  valid    = train_test_split(tmp_test,     test_size=0.5, random_state=42)
+    return train,test,valid
 
 def makeFolder(dirName):
     if not os.path.exists(dirName):
         os.mkdir(dirName)
 
-def splitData(seq,nstrands,pathSave):
-    nstrandsList = wrap(seq,nstrands)
+def getData(nstrandsList,pathSave):
     cont = 0
     for s in tqdm(nstrandsList):
         text_to_image.encode(s,os.path.join(pathSave,"strands_"+str(cont)+".png"))
@@ -30,16 +37,54 @@ if __name__ == "__main__":
     path_file_nCoV      = "/Users/nicolosavioli/Desktop/2019-nCoV/virus_genome/2019-nCoV.txt"
     path_file_HIV       = "/Users/nicolosavioli/Desktop/2019-nCoV/virus_genome/HIV.txt"
     # Dataset folder path 
-    path_dataset_fodler = "/Users/nicolosavioli/Desktop/corona_dataset"
-    makeFolder(path_dataset_fodler)
-    save_path_file_nCoV      = os.path.join(path_dataset_fodler,"positive")
-    save_path_file_HIV       = os.path.join(path_dataset_fodler,"negtive")
-    makeFolder(save_path_file_nCoV)
-    makeFolder(save_path_file_HIV)
-    nCoV                = readGenSeq(path_file_nCoV)
-    HIV                 = readGenSeq(path_file_HIV)
+    path_dataset_fodler = "/Users/nicolosavioli/Desktop/nCoV_dataset"
+    # Number of RNA vrius strands
     nstrands            = 20
-    print("\n ... Generete nCoV-2019 strands")
-    splitData(nCoV,nstrands,save_path_file_nCoV)
-    print("\n ... Generete HIV strands")
-    splitData(HIV,nstrands,save_path_file_HIV)
+    ##############################
+    nCoV_train,\
+    nCoV_test,\
+    nCoV_valid          = SplitStrands(path_file_nCoV,nstrands)
+    ##############################
+    HIV_train,\
+    HIV_test,\
+    HIV_valid           = SplitStrands(path_file_HIV,nstrands) 
+    ##############################
+    makeFolder(path_dataset_fodler)
+    ##############################
+    train_path = os.path.join(path_dataset_fodler,"train")
+    valid_path = os.path.join(path_dataset_fodler,"valid")
+    test_path  = os.path.join(path_dataset_fodler,"test")
+    ##############################
+    makeFolder(train_path)
+    makeFolder(valid_path)
+    makeFolder(test_path)
+    ############################## 
+    save_path_file_nCoV_train = os.path.join(train_path,"positive")
+    save_path_file_HIV_train  = os.path.join(train_path,"negtive")
+    makeFolder(save_path_file_nCoV_train)
+    makeFolder(save_path_file_HIV_train)
+    ##############################
+    save_path_file_nCoV_valid = os.path.join(valid_path,"positive")
+    save_path_file_HIV_valid  = os.path.join(valid_path,"negtive")
+    makeFolder(save_path_file_nCoV_valid)
+    makeFolder(save_path_file_HIV_valid)
+    ##############################
+    save_path_file_nCoV_test = os.path.join(test_path,"positive")
+    save_path_file_HIV_test  = os.path.join(test_path,"negtive")
+    makeFolder(save_path_file_nCoV_test)
+    makeFolder(save_path_file_HIV_test)
+    ##############################
+    print("\n ... Generete nCoV-2019 dataset train")
+    getData(nCoV_train,save_path_file_nCoV_train)
+    getData(HIV_train,save_path_file_HIV_train)
+    ###############################
+    print("\n ... Generete nCoV-2019 dataset valid")
+    getData(nCoV_valid,save_path_file_nCoV_valid)
+    getData(HIV_valid,save_path_file_HIV_valid)
+    ###############################
+    print("\n ... Generete nCoV-2019 dataset test")
+    getData(nCoV_test,save_path_file_nCoV_test)
+    getData(HIV_test,save_path_file_HIV_test)
+    ###############################
+
+
