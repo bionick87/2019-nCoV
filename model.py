58494e036058e853a207db6cd45beeacfce23142
Nicolò Savioli <nicolo.l.savioli@kcl.ac.uk>
2020-03-18 16:36:25 +0000

    first

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class SiameseNet(nn.Module):
         self.alex_net = models.alexnet(pretrained=True).features
         self.liner    = nn.Sequential(nn.Linear(12544, 4096), nn.Sigmoid())
         self.out      = nn.Linear(4096, 1)
-        self.softmax  = torch.nn.Softmax(dim=1)
+        self.softmax  = torch.nn.Softmax(dim=None)
         self.drop     = nn.Dropout(0.5)
 
     def cnn(self, x):
@@ -22,7 +22,7 @@ class SiameseNet(nn.Module):
     def forward(self, x1, x2):
         cnn1 = self.cnn(x1)
         cnn2 = self.cnn(x2)
-        dis  = self.drop(torch.abs(cnn1 - cnn2))
+        dis  = torch.abs(cnn1 - cnn2)
         out  = self.softmax(self.out(dis))
         return out
 
