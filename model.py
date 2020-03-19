edc43d65bc162dd544eb803caa1691ac3f557f82
Nicolò Savioli <nicolo.l.savioli@kcl.ac.uk>
2020-03-19 08:50:49 +0000

    first

--- model.py
+++ model.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 
-
+'''
 class SiameseNet(nn.Module):
     def __init__(self):
         super(SiameseNet, self).__init__()
         self.alex_net = models.alexnet(pretrained=True).features
         self.liner    = nn.Sequential(nn.Linear(12544, 4096), nn.Sigmoid())
         self.out      = nn.Linear(4096, 1)
-        
+
     def cnn(self, x):
         x = self.alex_net(x)
         x = x.view(x.size()[0],-1)
@@ -23,8 +23,10 @@ class SiameseNet(nn.Module):
         dis  = torch.abs(cnn1 - cnn2)
         out  = self.out(dis)
         return out
-
 '''
+
+
+
 class SiameseNet(nn.Module):
     def __init__(self):
         super(SiameseNet, self).__init__()
@@ -45,5 +47,5 @@ class SiameseNet(nn.Module):
         dis  = torch.abs(cnn1 - cnn2)
         out  = self.out(dis)
         return out
-'''
+
 
