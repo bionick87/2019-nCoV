cb6e09806b445df26b73ff0b81796274c8d7a5c0
Nicolò Savioli <nicolo.l.savioli@kcl.ac.uk>
2020-03-21 16:10:57 +0000

    first

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ class SiameseNet(nn.Module):
         return out
 
 '''
-
+'''
 class SiameseNet(nn.Module):
     def __init__(self):
         super(SiameseNet, self).__init__()
@@ -46,4 +46,26 @@ class SiameseNet(nn.Module):
         dis  = torch.abs(cnn1 - cnn2)
         out  = self.out(dis)
         return out
+'''
 
+
+class SiameseNet(nn.Module):
+    def __init__(self):
+        super(SiameseNet, self).__init__()
+        #  VGG model - in test
+        self.net       = models.wide_resnet50_2(pretrained=True).features
+        self.liner     = nn.Sequential(nn.Linear(32768, 4096), nn.Sigmoid())
+        self.out       = nn.Linear(4096, 1)
+
+    def cnn(self, x):
+        x = self.net(x)
+        x = x.view(x.size()[0],-1)
+        x = self.liner(x)
+        return x
+
+    def forward(self, x1, x2):
+        cnn1 = self.cnn(x1)
+        cnn2 = self.cnn(x2)
+        dis  = torch.abs(cnn1 - cnn2)
+        out  = self.out(dis)
+        return out
