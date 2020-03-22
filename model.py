8176c4202e30f6b9324790a297a997df9a57a5c0
nick <nick@bionick.home>
2020-03-22 11:03:49 +0000

    first

--- model.py
+++ model.py
@@ -48,12 +48,12 @@ class SiameseNet(nn.Module):
         return out
 '''
 
-'''
+
 class SiameseNet(nn.Module):
     def __init__(self):
         super(SiameseNet, self).__init__()
         #  VGG model - in test
-        resnet         = models.resnext50_32x4d(pretrained=True)
+        resnet         = models.wide_resnet101_2(pretrained=True)
         modules        = list(resnet.children())[:-1]      # delete the last fc layer.
         self.net       = nn.Sequential(*modules)
         self.liner     = nn.Sequential(nn.Linear(2048, 1024), nn.Sigmoid())
@@ -71,29 +71,7 @@ class SiameseNet(nn.Module):
         dis  = torch.abs(cnn1 - cnn2)
         out  = self.out(dis)
         return out
-'''
 
 
 
-class SiameseNet(nn.Module):
-    def __init__(self):
-        super(SiameseNet, self).__init__()
-        #  VGG model - in test
-        resnet         = models.inception_v3(pretrained=True)
-        modules        = list(resnet.children())[:-1]      # delete the last fc layer.
-        self.net       = nn.Sequential(*modules)
-        self.liner     = nn.Sequential(nn.Linear(1000, 512), nn.Sigmoid())
-        self.out       = nn.Linear(512, 1)
 
-    def cnn(self, x):
-        x = self.net(x)
-        x = x.view(x.size()[0],-1)
-        x = self.liner(x)
-        return x
-
-    def forward(self, x1, x2):
-        cnn1 = self.cnn(x1)
-        cnn2 = self.cnn(x2)
-        dis  = torch.abs(cnn1 - cnn2)
-        out  = self.out(dis)
-        return out
