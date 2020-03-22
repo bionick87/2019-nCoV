d4aa181593351a5d8d6b96258be698769f9ce813
nick <nick@bionick.home>
2020-03-22 12:39:25 +0000

    first

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 
-
+'''
 class SiameseNet(nn.Module):
     def __init__(self):
         super(SiameseNet, self).__init__()
@@ -23,7 +23,7 @@ class SiameseNet(nn.Module):
         dis  = torch.abs(cnn1 - cnn2)
         out  = self.out(dis)
         return out
-
+'''
 '''
 class SiameseNet(nn.Module):
     def __init__(self):
