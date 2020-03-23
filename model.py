151b124ac985715c60d7492fb9145b40504f34a5
nick <nick@bionick.home>
2020-03-23 08:29:47 +0000

    first

--- model.py
+++ model.py
@@ -33,10 +33,19 @@ class SiameseNet(nn.Module):
     def __init__(self):
         super(SiameseNet, self).__init__()
         #  VGG model - in test
-        self.net       = models.vgg13(pretrained=True)
-        self.net       = list(self.net.children())[:-2][0][:-13]  
-        self.liner     = nn.Sequential(nn.Linear(1048576, 4096))
-        self.out       = nn.Linear(4096, 1)
+        self.net         = models.vgg13(pretrained=True)
+        self.net         = list(self.net.children())[:-2][0][:-13]
+        ##########################################################
+        self.pre_weights_0 = net[0].weight      
+        self.net[0]        = nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=2)  
+        self.net[0].weight.data[:, :3, :, :] = self.pre_weights_0
+        ##########################################################
+        self.pre_weights_2 = net[2].weight      
+        self.net[2]        = nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=2)  
+        self.net[2].weight.data[:, :3, :, :] = self.pre_weights_2
+        ##########################################################
+        self.liner       = nn.Sequential(nn.Linear(1048576, 4096))
+        self.out         = nn.Linear(4096, 1)
 
     def cnn(self, x):
         x = self.net(x)
