1a35da2304aa75a5c5164eaeb97094109b2c1343
nick <nick@bionick.home>
2020-03-23 08:59:44 +0000

    first

--- model.py
+++ model.py
@@ -40,9 +40,9 @@ class SmallVGG(nn.Module):
         self.svgg[0]        = nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=2)  
         self.svgg[0].weight.data[:, :, :, :] = self.pre_weights_0
         ##########################################################
-        self.pre_weights_2 = self.svgg[2].weight      
-        self.svgg[2]        = nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=2).cuda()  
-        self.svgg[2].weight.data[:, :, :, :] = self.pre_weights_2
+        #self.pre_weights_2 = self.svgg[2].weight      
+        #self.svgg[2]        = nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=2).cuda()  
+        #self.svgg[2].weight.data[:, :, :, :] = self.pre_weights_2
     
     def forward(self, x):
         return self.svgg(x)
@@ -61,7 +61,6 @@ class SiameseNet(nn.Module):
     def cnn(self, x):
         x = self.net(x)
         x = x.view(x.size()[0],-1)
-        print(x.size())
         x = self.liner(x)
         return x
 
