512d0051bdadf1d114f95bdaafcc73680c6bed72
nick <nick@bionick.home>
2020-03-24 18:54:04 +0000

    first

--- paper/plot/main.py
+++ paper/plot/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
+import collections
+
 
 def movingaverage(interval, window_size):
     window= np.ones(int(window_size))/float(window_size)
@@ -17,24 +19,37 @@ def getPoints(fileName):
         clean.append(float(l.replace("\n", "")))  
     points   = np.array(clean).T
     x    = createList(0,len(points)-1)
-    y_av = movingaverage(points, 20)
-    return x,y_av
+    y_av = movingaverage(points, 10)
+    return  y_av
 
 
+#####################################################################
+file_alexnet        = "/home/nick/Desktop/results/alexnet/valid.txt"
+file_alexnet_np     = "/home/nick/Desktop/results/alexnet_no_pretrain/valid.txt"
+file_save           = "/home/nick/Desktop/code/2019-nCoV/paper/plot/plot.png"
+#####################################################################
+alexnet_pretrain    = getPoints(file_alexnet)
+alexnet_no_pretrain = getPoints(file_alexnet_np) 
 
 
-
-
+print(alexnet_pretrain)
+#####################################################################
 fig            = plt.figure()
 ax             = fig.add_subplot(111)
 #ax.set_title   ("Validset sensitivity")
-ax.plot        (alexnet_pretrain, '-',  label="Sensitivity",color='r')
-ax.plot        (alexnet_no_pretrain, '-',  label="Sensitivity",color='r')
+ax.plot        (alexnet_pretrain, '-',  label="Alexnet pretrain",color='r')
+ax.plot([np.mean(alexnet_pretrain)]*len(alexnet_pretrain), linestyle='--', color='r')
+
+
+ax.plot        (alexnet_no_pretrain, '-',  label="Alexnet",color='g')
+ax.plot        ([np.mean(alexnet_no_pretrain)]*len(alexnet_no_pretrain), linestyle='--', color='g')
+
+
 ax.set_ylabel  ('Sensitivity (%)')
-ax.set_xlabel  ("Epochs (x100)")
+ax.set_xlabel  ("Epochs (x10)")
 ax.legend      (loc='lower right')
 handles, labels = plt.gca().get_legend_handles_labels()
-by_label        = OrderedDict(zip(labels, handles))
+by_label        = collections.OrderedDict(zip(labels, handles))
 plt.legend     (by_label.values(), by_label.keys())
-fig.savefig    (os.path.join             (pathSave, "valid.jpg"))
+fig.savefig    (file_save)
 
